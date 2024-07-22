ab53aeb87a197818118913717212cafa59fa3609
백지윤 <wendy6992@naver.com>
2024-07-22 13:23:02 +0900

    Feat: Add examination panel logic

--- Assets/Scripts/Object/ExaminationPanel.cs
+++ Assets/Scripts/Object/ExaminationPanel.cs
@@ -2,20 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ExaminationPanel : MonoBehaviour
 {
     public Button examinationButton;
+    public Button startExaminationButton;
     public NPCInteraction npcInteraction;
     public Animator bedAnimator;
     public float wheelDistance;
     public GameObject wheelchair;
     public Transform bedTransform; // Add a reference to the bed's transform
     public TriggerArea triggerArea; // Add a reference to TriggerArea
+    public GameObject resultPanel; // Add a reference to the result panel
 
     void Start()
     {
         examinationButton.onClick.AddListener(OnExaminationButtonClick);
+        startExaminationButton.onClick.AddListener(OnStartExaminationButtonClick);
+
+        if (resultPanel != null)
+        {
+            resultPanel.SetActive(false); // Initially hide the result panel
+        }
+
     }
 
     private bool IsNearby()
@@ -38,10 +48,32 @@ public class ExaminationPanel : MonoBehaviour
             {
                 triggerArea.ExitInteraction();
             }
+
         }
         else
         {
             Debug.Log("아직 할당 안됨");
+        }
+    }
+
+    private void OnStartExaminationButtonClick()
+    {
+        if (npcInteraction.transform.IsChildOf(bedTransform))
+        {
+            bedAnimator.SetTrigger("Bed_In");
+            DisplayExaminationResults();
+        }
+    }
+
+    private void DisplayExaminationResults()
+    {
+        if (resultPanel != null)
+        {
+            resultPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.LogError("Result panel is not assigned.");
         }
     }
 }
