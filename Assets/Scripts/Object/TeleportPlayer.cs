a406595a0065ee79a56eb8c55bfb8ff2e109c731
이민지 <50236187+namoo1818@users.noreply.github.com>
2024-07-25 10:05:21 +0900

    feat: 탈출정 콜라이더 설정 및 텔레포트 스크립트 적용

--- Assets/Scripts/Object/TeleportPlayer.cs
+++ Assets/Scripts/Object/TeleportPlayer.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class TeleportPlayer : MonoBehaviour
 {
-    public Transform teleportTarget;
-    public GameObject player;
+    public Transform teleportTarget; // 텔레포트할 위치
+    public GameObject player; // 플레이어 오브젝트
+    public KeyCode teleportKey = KeyCode.R; // 텔레포트 키 설정
+
+    private bool canTeleport = false; // 텔레포트 가능 여부
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject == player)
+        {
+            canTeleport = true; // 플레이어가 트리거에 들어왔을 때 텔레포트 가능
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            canTeleport = false; // 플레이어가 트리거를 벗어났을 때 텔레포트 불가능
+        }
+    }
+
+    void Update()
+    {
+        if (canTeleport && Input.GetKeyDown(teleportKey))
         {
             Teleport();
         }
