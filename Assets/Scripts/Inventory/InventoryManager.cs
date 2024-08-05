5c27e67686455b6a84e034a0bada89878b09c985
KTG <xorb269@naver.com>
2024-08-06 04:06:00 +0900

    Fix: 연구실 잔버그 수정

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -185,7 +185,7 @@ public class InventoryManager : MonoBehaviour
         pickedItemPosition.SetActive(true);
     }
 
-    public void PutItem(Transform putItemPosition)
+    public Collectable PutItem(Transform putItemPosition)
     {
         
         Collectable obj = collectables[pickedItemIndex];
@@ -201,6 +201,8 @@ public class InventoryManager : MonoBehaviour
 
         DestroyItem(pickedItemIndex);
         DeselectItem();
+
+        return putItem.GetComponent<Collectable>();
     }
 
     public void DropItem(int index, Vector3 dropPosition)
--- Assets/Scripts/Object/Interactable/Putable.cs
+++ Assets/Scripts/Object/Interactable/Putable.cs
@@ -1,3 +1,4 @@
+using Meta.WitAi;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -34,9 +35,7 @@ public class Putable : MonoBehaviour
     {
         if (inventoryManager.pickedItemIndex != -1)
         {
-            Collectable obj = inventoryManager.GetItemByIndex(inventoryManager.pickedItemIndex);
-            putItem = obj;
-            inventoryManager.PutItem(putItemPosition);
+            putItem = inventoryManager.PutItem(putItemPosition);
             guideText.gameObject.SetActive(false);
         }
     }
