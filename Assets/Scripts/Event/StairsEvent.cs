1a42a61b92218e4f70b9e2d6cd53d8d5cfb6208c
Starbow-Break <phsyobs@naver.com>
2024-08-05 23:10:51 +0900

    ADD : 제어실 UI 상호작용 VR 적용

--- Assets/Scripts/Event/StairsEvent.cs
+++ Assets/Scripts/Event/StairsEvent.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class StairsEvent : MonoBehaviour
 {
     [SerializeField]
-    Camera mainCamera; // 메인 카메라
+    GameObject rig; // Rig
 
     [SerializeField]
-    Camera targetCamera; // 목표 카메라
+    GameObject targetPos; // Rig의 목표 위치
 
     [SerializeField]
     FadePanel fadePanel; // 페이드 판넬
 
     [SerializeField]
     Stairs stairs; // 계단
+
+    [SerializeField]
+    public OVRPlayerController playerController;
 
     bool isPlaying = false; // 이벤트 재생 상태
 
@@ -27,12 +30,19 @@ public class StairsEvent : MonoBehaviour
     IEnumerator PlayEventFlow() {
         isPlaying = true;
 
+        // 플레이어 컨트롤러 비활성화
+        playerController.enabled = false;
+
         // Scene Fade Out
         yield return fadePanel.FadeOut();
 
+        // 메인 카메라의 위치와 회전 상태 저장
+        Vector3 rigPos = rig.transform.position;
+        Quaternion rigRot = rig.transform.rotation;
+
         // 카메라 전환
-        mainCamera.enabled = false;
-        targetCamera.enabled = true;
+        rig.transform.position = targetPos.transform.position;
+        rig.transform.rotation = targetPos.transform.rotation;
 
         // Scene Fade In
         yield return fadePanel.FadeIn();
@@ -45,12 +55,14 @@ public class StairsEvent : MonoBehaviour
         // Scene Fade Out
         yield return fadePanel.FadeOut();
 
-        // 카메라 전환
-        mainCamera.enabled = true;
-        targetCamera.enabled = false;
+        // 원래 위치로 복귀
+        rig.transform.position = rigPos;
+        rig.transform.rotation = rigRot;
 
         // Scene Fade In
         yield return fadePanel.FadeIn();
+
+        playerController.enabled = true;
 
         isPlaying = false;
     }
