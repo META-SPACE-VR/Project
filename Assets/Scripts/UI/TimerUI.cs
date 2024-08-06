088729d55476104de54cf1ec38ab73f1f1e32aae
STARBOW <starbow.dev@gmail.com>
2024-08-06 11:47:41 +0900

    MOD : 타이머 로직 수정

--- Assets/Scripts/UI/TimerUI.cs
+++ Assets/Scripts/UI/TimerUI.cs
@@ -4,26 +4,19 @@ using TMPro;
 public class TimerTMP : MonoBehaviour
 {
     public TextMeshProUGUI timerText; // TMP Text 요소를 할당받기 위한 변수
-    private float elapsedTime = 0f; // 경과 시간
+
+    [SerializeField]
+    Timer timer;
 
     // 게임이 시작될 때 호출되는 메서드
     void Start()
     {
-        elapsedTime = 0f;
+        timerText.text = "00:00:00.000";
     }
 
     // 매 프레임마다 호출되는 메서드
     void Update()
     {
-        // 경과 시간 업데이트
-        elapsedTime += Time.deltaTime;
-
-        // 시간을 시, 분, 초로 변환
-        int hours = Mathf.FloorToInt(elapsedTime / 3600f);
-        int minutes = Mathf.FloorToInt((elapsedTime % 3600) / 60f);
-        int seconds = Mathf.FloorToInt(elapsedTime % 60);
-
-        // UI 텍스트 업데이트
-        timerText.text = string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+        timerText.text = timer.GetTimeString();
     }
 }
