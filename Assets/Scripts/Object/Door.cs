d4f8914bc2acb4061a51522196d84d3e7274b956
KTG <xorb269@naver.com>
2024-08-06 19:41:28 +0900

    Feat: 개인실 문 열림상호작용

--- Assets/Scripts/Object/Door.cs
+++ Assets/Scripts/Object/Door.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public Animator anim;
+
+    /*
     public Transform player;
     public Transform door;
-
 
     // Update is called once per frame
     void Update()
@@ -19,6 +20,23 @@ public class Door : MonoBehaviour
             anim.SetBool("character_nearby", true);
         }
         else
+        {
+            anim.SetBool("character_nearby", false);
+        }
+    }
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.SetBool("character_nearby", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
             anim.SetBool("character_nearby", false);
         }
