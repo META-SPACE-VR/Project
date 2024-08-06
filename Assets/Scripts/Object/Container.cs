d472ca80f347aad2aa83720753aa818cd7baff52
STARBOW <starbow.dev@gmail.com>
2024-08-06 17:52:19 +0900

    FIX : 컨테이너 퍼즐 로직 수정

--- Assets/Scripts/Object/Container.cs
+++ Assets/Scripts/Object/Container.cs
@@ -6,15 +6,34 @@ public class Container : MonoBehaviour
 {
     RobotArm robotArm;
 
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
         robotArm = GameObject.FindGameObjectWithTag("Robot Arm").GetComponent<RobotArm>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // 키네마틱 상태로 변경
+    public void changeKinematic() {
+        rb.useGravity = false;
+        rb.isKinematic = true;
+    }
+
+    // 비 키네마틱 상태로 변경
+    public void changeNonKinematic(bool useGravity = true) {
+        rb.useGravity = useGravity;
+        rb.isKinematic = false;
+    }
+
+    public bool IsKinematic() {
+        return rb.isKinematic;
     }
 
     private void OnCollisionEnter(Collision other) {
         // 다른 컨테이너랑 충돌 시 이동 초기화
-        if(other.gameObject.CompareTag("Container")) {
+        if(other.gameObject.CompareTag("Container") || other.gameObject.CompareTag("Player")) {
             robotArm.ResetMove();
         }
     }
@@ -22,6 +41,10 @@ public class Container : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("Wall")) {
             robotArm.ResetMove();
+        }
+
+        if(other.gameObject.CompareTag("Floor")) {
+            changeKinematic();
         }
     }
 }
